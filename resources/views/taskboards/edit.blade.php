abdcf70fcd5ed2a6fa31cfecf65d05263d5262fe
EC2 Default User <ec2-user@ip-172-31-8-220.us-east-2.compute.internal>
2018-05-23 02:29:02 +0000

    bootstrap design

--- resources/views/taskboards/edit.blade.php
+++ resources/views/taskboards/edit.blade.php
@@ -4,16 +4,22 @@
 
     <h1>id: {{ $taskboard->id }} のタスク編集ページ</h1>
     
-    {!! Form::model($taskboard, ['route' => ['taskboards.update', $taskboard->id], 'method' => 'put']) !!}
+    
+    <div class="row">
+        <div class="col-xs-6">
+            {!! Form::model($taskboard, ['route' => ['taskboards.update', $taskboard->id], 'method' => 'put']) !!}
+            <div class="form-group">
+                {!! Form::label('content', 'タスク:') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            </div>
+                
+            <div class="form-group">
+                {!! Form::label('status', 'ステータス') !!}
+                {!! Form::text('status', null, ['class' => 'form-control']) !!}
+            </div>
+            
+            {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
 
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-        
-        {!! Form::label('status', 'ステータス') !!}
-        {!! Form::text('status') !!}
-
-        {!! Form::submit('更新') !!}
-
-    {!! Form::close() !!}
-
+        {!! Form::close() !!}
+    </div>
 @endsection
