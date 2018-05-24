3b64fe8b3215b6cbba3fbadfb212336cb31eda34
EC2 Default User <ec2-user@ip-172-31-8-220.us-east-2.compute.internal>
2018-05-24 01:55:52 +0000

    edit bootstrap

--- resources/views/taskboards/edit.blade.php
+++ resources/views/taskboards/edit.blade.php
@@ -6,7 +6,7 @@
     
     
     <div class="row">
-        <div class="col-xs-6">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
             {!! Form::model($taskboard, ['route' => ['taskboards.update', $taskboard->id], 'method' => 'put']) !!}
             <div class="form-group">
                 {!! Form::label('content', 'タスク:') !!}
@@ -18,7 +18,7 @@
                 {!! Form::text('status', null, ['class' => 'form-control']) !!}
             </div>
             
-            {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
+            {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
 
         {!! Form::close() !!}
     </div>
